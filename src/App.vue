d4f3b9adbbf468b4771efad1943dba54d584e4c7
s8wecy <kontakt@julian-claus.de>
2018-08-31 07:23:45 +0200

    chore: first commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,66 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <slide :steps="4">
+            <article>
+                <section v-if="step === 1" class="bg-purple aligncenter">
+                    <span class="background dark"
+                          style="background-image:url('/images/slides/rawpixel-558599-unsplash.jpg')"></span>
+                    <div class="wrap">
+                        <h1 class="text-landing">Modern Web Development with Vue.js</h1>
+                        <p class="text-intro">State of the Art. Vue.js Stärken. Ausblick.</p>
+                    </div>
+                </section>
+
+                <section v-if="step >= 2" class="slide-top">
+                    <div class="wrap">
+                        <div class="content-center">
+                            <h2 class="text-landing">Features</h2>
+                            <p><code>ul.flexblock</code> = Flexible blocks<br> with auto-fill and equal height.</p>
+                        </div>
+                        <hr>
+                        <ul class="flexblock">
+                            <li>
+                                <h3>
+                                    User Interfaces
+                                </h3>
+                                Wartbar, schnell anpassbar, nutzerfreundlich
+                            </li>
+                            <li>
+                                <h2>
+                                    .flexblock li 2
+                                </h2>
+                                Multipurpose: benefits, clients, work...
+                            </li>
+                            <li>
+                                <h2>
+                                    .flexblock li 3
+                                </h2>
+                                Multipurpose: news, metrics, plans...
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+            </article>
+        </slide>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
+
+<script>
+import { Slideshow } from 'eagle.js'
+
+export default {
+  mixins: [Slideshow],
+
+  data: () => ({
+    product: {
+      title: 'Jeans',
+      description: '5-pocket low-rise jeans in washed stretch denim with a button fly and slim legs. The jeans are made partly from recycled cotton.',
+      price: 64,
+      type: ''
+    }
+  })
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Eagle from 'eagle.js'
+import 'eagle.js/dist/eagle.css'
+// import animate.css for slide transition
+import 'animate.css'
+
+Vue.use(Eagle)
 
 Vue.config.productionTip = false
 
